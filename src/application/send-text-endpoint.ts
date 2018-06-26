8ce33c8588760189914a3d57cbb245180bf792c9
Artur Chyży <artur.chyzy@pragmatists.pl>
2018-06-26 15:36:43 +0200

    Send text message

--- src/application/send-text-endpoint.ts
+++ src/application/send-text-endpoint.ts
@@ -1,25 +1,28 @@
-import { NextFunction, Request, Response, Router } from 'express';
+import { NextFunction, Request, Response, Router } from "express";
+import { SendTextService } from "../infrastructure/scg/send-text.service";
 
 class SendTextEndpoint {
-  readonly router: Router;
+    readonly router: Router;
+    private readonly sendTextService: SendTextService;
 
-  constructor() {
-    this.router = Router();
-    this.init();
-  }
+    constructor() {
+        this.router = Router();
+        this.init();
+        this.sendTextService = new SendTextService();
+    }
 
-  private sendText(req: Request, res: Response, next: NextFunction) {
-      console.log("send");
-    res.json({});
-  }
+    private sendText(req: Request, res: Response, next: NextFunction) {
+        this.sendTextService.sendText(req.body.to, req.body.text);
+        res.json({});
+    }
 
 
-  private init() {
-    this.router.post(
-      '/',
-      (req, res, next) => this.sendText(req, res, next)
-    );
-  }
+    private init() {
+        this.router.post(
+            "/",
+            (req, res, next) => this.sendText(req, res, next)
+        );
+    }
 }
 
 export default new SendTextEndpoint().router;
---
+++ src/infrastructure/scg/send-text.service.ts
@@ -0,0 +1,29 @@
+import axios from "axios";
+
+export class SendTextService {
+
+    constructor() {
+    }
+
+    public sendText(to: string, text: string) {
+        axios.post("https://developer.syniverse.com/scg-external-api/api/v1/messaging/message_requests",
+            {
+                body: text,
+                from: "sender_id:nX6HiGTf7YEMjorAhwBDH",
+                pause_before_transmit: false,
+                sender_id_sort_criteria: [],
+                to: [to]
+            },
+            {headers: {Authorization: "Bearer 7c382c26-174d-37f4-bc25-4c30d866d758"}})
+            .then((resp) => {
+
+                console.log("received resp", resp.data);
+
+
+            }).catch((err) => {
+            console.log("Error: ", err);
+        });
+    }
+
+
+}
